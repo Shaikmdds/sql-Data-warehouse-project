215b3edff0ddd7577359267b51201f3d9f0456b9
Shaikmdds <112384078+Shaikmdds@users.noreply.github.com>
2025-05-14 02:27:34 -0500

    Update ddl_silver.sql

--- scripts/Silver/ddl_silver.sql
+++ scripts/Silver/ddl_silver.sql
@@ -1,3 +1,14 @@
+/*
+======
+DDL Script: Create Silver Tables
+Script Purpose:
+
+This script creates tables in the 'Silver' schema, dropping existing tables if they already exist.
+Run this script to re-define the DDL structure of 'Silver' Tables
+======
+*/
+
+
 if OBJECT_ID ('silver.crm_cust_info', 'U') is not null
    drop table silver.crm_cust_info;
 create table silver.crm_cust_info(
